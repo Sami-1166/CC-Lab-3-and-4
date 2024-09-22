d041c652fcaae669d401863056a275140e5db2d2
Sami-1166 <sami.613619@gmail.com>
2024-09-22 18:43:28 +0500

    Update Form1.cs

--- Lab 3 Task 2/Lab 3 Task 2/Form1.cs
+++ Lab 3 Task 2/Lab 3 Task 2/Form1.cs
@@ -16,21 +16,15 @@ namespace Lab_3_Task_2
 
         private void button1_Click(object sender, EventArgs e)
         {
-            // Get the text from the TextBox (replace 'textBox1' with the actual name of your TextBox)
             string input = textBox1.Text;
-
-            // Regular expression to match scientific notation format
             string pattern = @"^-?\d+(\.\d+)?[eE][-+]?\d+$";
 
-            // Validate the input using the regular expression
             if (Regex.IsMatch(input, pattern))
             {
-                // Show success message
                 MessageBox.Show("Success! The input is in correct scientific notation format.", "Validation", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
             else
             {
-                // Show error message
                 MessageBox.Show("Invalid input. Please enter a number in scientific notation format like 8e4 or 5e-2.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
         }
